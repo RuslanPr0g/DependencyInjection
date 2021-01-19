fb162040bd3fa7706d3f8199bebef27f820d3eab
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-19 14:19:46 +0300

    init commit

---
+++ Dependency Injection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dependency_Injection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

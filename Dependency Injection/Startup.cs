90855880d21d35fd682bb924b390716fb73cbb87
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-19 14:52:41 +0300

    add startup dependency configuration builder

---
+++ Dependency Injection/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dependency_Injection
+{
+    public static class Startup
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            return builder.Build();
+        }
+    }
+}

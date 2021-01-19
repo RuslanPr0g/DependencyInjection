26eb31cf0bdae94ae787bfef9d4a07ccd1450248
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-19 14:59:55 +0300

    register assembly types (add as statement)

--- Dependency Injection/Startup.cs
+++ Dependency Injection/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 using ZabavnayaBiblioteka;
@@ -16,7 +17,8 @@ namespace Dependency_Injection
             builder.RegisterType<BusinessLogic>().As<IBusinessLogic>();
 
             builder.RegisterAssemblyTypes(Assembly.Load(nameof(ZabavnayaBiblioteka)))
-                .Where(t => t.Namespace.Contains("Utilities"));
+                .Where(t => t.Namespace.Contains("Utilities"))
+                .As(t => t.GetInterfaces().FirstOrDefault(i => i.Name == "I" + t.Name));
 
             return builder.Build();
         }
